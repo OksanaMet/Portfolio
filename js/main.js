06987e3882b33af4638cdd972de347ed0fd6ba62
Oksana Met <Omet92@gmail.com>
2022-07-05 17:37:27 +0200

    feat: add burger menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@
         }
     };
 }());
+
+    (function () {
+        const burgerItem = document.querySelector('.burger');
+        const menu = document.querySelector('.header__nav');
+        const menuCloseItem = document.querySelector('.header__nav-close');
+        burgerItem.addEventListener('click', () => {
+            menu.classList.add('header__nav_active');
+        });
+        menuCloseItem.addEventListener('click', () => {
+            menu.classList.remove('header__nav_active');
+        })
+    } ());
