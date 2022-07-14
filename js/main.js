a1d038199219d53c97d364c79ef60d1158ba6c09
Oksana Met <Omet92@gmail.com>
2022-07-14 19:35:06 +0200

    refactor: delete some img, rename some img.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function () {
     const header = document.querySelector('.header');
+
     window.onscroll = () =>  {
         if (window.pageYOffset > 50) {
             header.classList.add('header_active');
@@ -13,12 +14,20 @@
         const burgerItem = document.querySelector('.burger');
         const menu = document.querySelector('.header__nav');
         const menuCloseItem = document.querySelector('.header__nav-close');
+        const menuLink = document.querySelectorAll('.header__link')
         burgerItem.addEventListener('click', () => {
             menu.classList.add('header__nav_active');
         });
         menuCloseItem.addEventListener('click', () => {
             menu.classList.remove('header__nav_active');
         })
+        if(window.innerWidth < 768){
+            for (let i = 0; i < menuLink.length; i+=1 ){
+                menuLink[i].addEventListener('click', () => {
+                    menu.classList.remove('header__nav_active')
+                })
+            }
+        }
     } ());
     
     (function () {
